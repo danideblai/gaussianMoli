d3ab1ed82cbaa25b70788624bb6dc96c1482ae20
danideblai <danielsam55@gmail.com>
2025-04-10 01:54:28 +0200

    fgfgd

---
+++ OrbitControls.js
@@ -0,0 +1,3 @@
+
+// OrbitControls.js
+export { OrbitControls } from 'https://unpkg.com/three@0.150.1/examples/jsm/controls/OrbitControls.js';
---
+++ three.module.js
@@ -0,0 +1,3 @@
+
+// three.module.js (simplificado)
+export * from 'https://unpkg.com/three@0.150.1/build/three.module.js';
