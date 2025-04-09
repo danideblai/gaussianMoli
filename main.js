1527e8152b8eda5934a621d72d15416605fae94b
danideblai <danielsam55@gmail.com>
2025-04-10 00:31:45 +0200

    nnn

--- main.js
+++
@@ -1,9 +0,0 @@
-
-import { initViewer } from './viewer-final.js?v=3';
-
-window.addEventListener('DOMContentLoaded', async () => {
-  await initViewer({
-    modelPath: 'Moli02.ply',
-    cameraCsvPath: 'realitymoli.csv'
-  });
-});
